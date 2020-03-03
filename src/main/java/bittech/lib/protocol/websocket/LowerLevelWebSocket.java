763718dfaf6a8429cc20d54a0db4f09f63063383
asocjal <mailtocd@wp.pl>
2020-03-03 11:31:38 +0100

    Improvements in WebSocketClient

---
+++ src/main/java/bittech/lib/protocol/websocket/LowerLevelWebSocket.java
@@ -0,0 +1,64 @@
+package bittech.lib.protocol.websocket;
+
+import java.net.URI;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import bittech.lib.protocol.Command;
+import bittech.lib.utils.Require;
+import bittech.lib.utils.exceptions.StoredException;
+
+public class LowerLevelWebSocket extends WebSocketClient {
+
+	private final Object connected = new Object();
+	private final JsonCommandExec exec;
+
+	public LowerLevelWebSocket(JsonCommandExec exec, URI serverURI) {
+		super(serverURI);
+		try {
+			this.exec = Require.notNull(exec, "exec");
+			connect();
+			synchronized (connected) {
+				connected.wait();
+			}
+		} catch (Exception ex) {
+			throw new StoredException("Cannot create WebSocket connector", ex);
+		}
+	}
+
+	@Override
+	public void onOpen(ServerHandshake handshakedata) {
+		System.out.println("opened connection");
+		synchronized (connected) {
+			connected.notify();
+		}
+	}
+
+	@Override
+	public void onMessage(String message) {
+		if ("alive".equals(message)) {
+			return;
+		}
+		exec.onReceived("", message);
+	}
+
+	@Override
+	public void onClose(int code, String reason, boolean remote) {
+		System.out.println(
+				"Connection closed by " + (remote ? "remote peer" : "us") + " Code: " + code + " Reason: " + reason);
+	}
+
+	@Override
+	public void onError(Exception ex) {
+		new StoredException("Web socket error", ex);
+	}
+
+	public void send(String authKey, Command<?, ?> command) {
+		String cmdStr = exec.prepareToSend(authKey, command);
+//		System.out.println("Sending: " + cmdStr);
+		send(cmdStr);
+		exec.waitForResponse(command);
+	}
+
+}
---
+++ src/main/java/bittech/lib/protocol/websocket/WebSocketClient.java
@@ -0,0 +1,35 @@
+package bittech.lib.protocol.websocket;
+
+import java.net.URI;
+
+import bittech.lib.protocol.Command;
+import bittech.lib.protocol.Listener;
+import bittech.lib.protocol.ListenersManager;
+import bittech.lib.utils.exceptions.StoredException;
+
+public class WebSocketClient {
+
+	LowerLevelWebSocket exampleClient;
+
+	ListenersManager lisMan = new ListenersManager();
+	JsonCommandExec exec = new JsonCommandExec(lisMan);
+
+	public WebSocketClient(String serverUri) {
+		try {
+			lisMan = new ListenersManager();		
+			exec = new JsonCommandExec(lisMan);
+			exampleClient = new LowerLevelWebSocket(exec, new URI(serverUri));
+		} catch (Exception ex) {
+			throw new StoredException("Cannot create WebSocketTester", ex);
+		}
+	}
+	
+	public void registerListener(Listener listener) {
+		lisMan.registerListener(listener);
+	}
+
+	public void send(String authKey, Command<?, ?> command) {
+		exampleClient.send(authKey, command);
+	}
+
+}
