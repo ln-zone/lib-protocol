50d214c7b72094446b0ea2ab4a6553aa323e8890
asocjal <mailtocd@wp.pl>
2020-09-28 14:41:26 +0200

    WiP

--- src/main/java/bittech/lib/protocol/websocket/WebSocketClient.java
+++ src/main/java/bittech/lib/protocol/websocket/WebSocketClient.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import bittech.lib.protocol.Command;
 import bittech.lib.protocol.Listener;
 import bittech.lib.protocol.ListenersManager;
+import bittech.lib.utils.Utils;
 import bittech.lib.utils.exceptions.StoredException;
 
 public class WebSocketClient {
@@ -16,20 +17,21 @@ public class WebSocketClient {
 
 	public WebSocketClient(String serverUri) {
 		try {
-			lisMan = new ListenersManager();		
+			lisMan = new ListenersManager();
 			exec = new JsonCommandExec(lisMan);
 			exampleClient = new LowerLevelWebSocket(exec, new URI(serverUri));
 		} catch (Exception ex) {
 			throw new StoredException("Cannot create WebSocketTester", ex);
 		}
 	}
-	
+
 	public void registerListener(Listener listener) {
 		lisMan.registerListener(listener);
 	}
 
 	public void send(String authKey, Command<?, ?> command) {
 		exampleClient.send(authKey, command);
+		Utils.prn(command);
 	}
 
 }
